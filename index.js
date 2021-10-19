5274840afca04339522a46ccb667337f40be1a18
amineslamizadeh <88544287+amineslamizadeh@users.noreply.github.com>
2021-10-19 13:46:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const axios = require('axios')
 
 function analiz()
 {
-    axios
-        .get('https://bidex.pw/appSignal/ajax/n_signals.php?noee=~@Tedi1377./')
-        .then(res => {
-            data=res.data;
+   
             console.log("asd");
 
-        })
-        .catch(error => {
-            console.error(error)
-        })
+        
 }
 
 analiz();
